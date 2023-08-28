837949d2ed0d92c1f6ba7ab241de064d53498a16
Flavio Barbosa <flavio.barbosa@dataprev.gov.br>
2023-08-28 10:59:07 -0300

    test: create find by id repository

--- src/main/java/com/github/flaviobarbosa/Todo/domain/model/Todo.java
+++ src/main/java/com/github/flaviobarbosa/Todo/domain/model/Todo.java
@@ -6,6 +6,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import java.util.Objects;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -36,4 +37,20 @@ public class Todo {
   @Column(name = "done", nullable = false)
   private boolean done;
 
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    Todo todo = (Todo) o;
+    return Objects.equals(id, todo.id);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id);
+  }
 }
--- src/test/java/com/github/flaviobarbosa/Todo/domain/repository/TodoRepositoryTest.java
+++ src/test/java/com/github/flaviobarbosa/Todo/domain/repository/TodoRepositoryTest.java
@@ -5,6 +5,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import com.github.flaviobarbosa.Todo.DatabaseTest;
 import com.github.flaviobarbosa.Todo.domain.model.Todo;
+import java.util.Optional;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -22,6 +24,7 @@ class TodoRepositoryTest extends DatabaseTest {
   private TestEntityManager testEntityManager;
 
   @Test
+  @DisplayName("Should save a new Todo")
   public void shouldSaveTodo() {
     Todo todo = todoRepository.save(TODO_WITHOUT_ID);
 
@@ -32,5 +35,18 @@ class TodoRepositoryTest extends DatabaseTest {
     assertThat(sut.getDescription()).isEqualTo(TODO_WITHOUT_ID.getDescription());
     assertThat(sut.getId()).isNotNull();
 
+  }
+
+  @Test
+  @DisplayName("Should find Todo by id")
+  public void shouldFindById() {
+    Todo todo = todoRepository.save(TODO_WITHOUT_ID);
+
+    Optional<Todo> optionalTodo = todoRepository.findById(todo.getId());
+
+    Todo sut = testEntityManager.find(Todo.class, todo.getId());
+
+    assertThat(optionalTodo.isPresent());
+    assertThat(optionalTodo.get()).isEqualTo(sut);
   }
 }
