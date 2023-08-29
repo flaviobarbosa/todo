7f94c3dabb73fcfd4ceeaa7a757e951d50fe00d4
Flavio Barbosa <flaviombarbosa@gmail.com>
2023-08-29 11:58:05 -0300

    refactor: move classes to correct packages

--- src/main/java/com/github/flaviobarbosa/Todo/domain/exception/TodoNotFoundException.java
+++ src/main/java/com/github/flaviobarbosa/Todo/domain/exception/TodoNotFoundException.java
@@ -1,5 +1,8 @@
 package com.github.flaviobarbosa.Todo.domain.exception;
 
+import lombok.Data;
+
+@Data
 public class TodoNotFoundException extends RuntimeException {
 
     public TodoNotFoundException(int id) {
