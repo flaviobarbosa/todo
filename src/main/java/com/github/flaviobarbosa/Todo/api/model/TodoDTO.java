a267664860722c536e6a2948427395f250cfede1
Flavio Barbosa <flaviombarbosa@gmail.com>
2023-09-04 14:38:25 -0300

    test: create integration tests

--- src/main/java/com/github/flaviobarbosa/Todo/api/model/TodoDTO.java
+++ src/main/java/com/github/flaviobarbosa/Todo/api/model/TodoDTO.java
@@ -1,12 +1,16 @@
 package com.github.flaviobarbosa.Todo.api.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Builder
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class TodoDTO {
 
   private int id;
